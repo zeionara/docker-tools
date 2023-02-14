f85324390a9d5fc63e8fd0b3032689c5e841f985
dima <zeionara@niuitmo.ru>
2023-02-14 22:23:35 +0000

    Added port printing to the verbose for of driis alias

--- inspect/ip-address-using-image-repo-tag-substring.sh
+++ inspect/ip-address-using-image-repo-tag-substring.sh
@@ -3,11 +3,14 @@
 containers=($(sudo docker ps -q | xargs sudo docker inspect --format="{{.Id}}"))
 images=($(sudo docker ps -q | xargs sudo docker inspect --format="{{.Image}}"))
 
+port_format=$(echo '{{ range $key, $value := .NetworkSettings.Ports }}{{$key}} <- {{ range $value }}{{ .HostIp }}:{{ .HostPort }} {{ end }}{{ end }}')
+
 for i in ${!containers[@]}; do
     tags=$(docker inspect ${images[$i]} --format "{{range .RepoTags}}{{.}}{{end}}")
     if grep -q "$1" <<< $tags; then
         if [ "$2" == '-v' ]; then
-            sudo docker inspect ${containers[$i]} --format="{{ range \$key, \$value := .NetworkSettings.Networks}}{{\$value.IPAddress}} {{\$key}} $tags{{end}}"
+            ports=$(sudo docker inspect ${containers[$i]} --format="$port_format")
+            sudo docker inspect ${containers[$i]} --format="{{ range \$key, \$value := .NetworkSettings.Networks}}{{\$value.IPAddress}} {{\$key}} $tags $ports{{end}}"
         else
             sudo docker inspect ${containers[$i]} --format="{{ range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}"
         fi
