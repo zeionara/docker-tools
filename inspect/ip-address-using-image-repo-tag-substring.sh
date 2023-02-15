29cf294aaefd37982343a77ad1cc3e6555c40ded
dima <zeionara@niuitmo.ru>
2023-02-15 18:10:34 +0000

    Fixed ip addresses inspection script

--- inspect/ip-address-using-image-repo-tag-substring.sh
+++ inspect/ip-address-using-image-repo-tag-substring.sh
@@ -8,8 +8,8 @@ images=($(sudo docker ps -q | xargs sudo docker inspect --format="{{.Image}}"))
 port_format=$(echo '{{ range $key, $value := .NetworkSettings.Ports }}{{$key}} <- {{ range $value }}{{ .HostIp }}:{{ .HostPort }} {{ end }}{{ end }}')
 
 for i in ${!containers[@]}; do
+    tags=$(docker inspect ${images[$i]} --format "{{range .RepoTags}}{{.}}{{end}}")
     if [[ -z $1 ]]; then
-        tags=$(docker inspect ${images[$i]} --format "{{range .RepoTags}}{{.}}{{end}}")
         ports=$(sudo docker inspect ${containers[$i]} --format="$port_format")
         sudo docker inspect ${containers[$i]} --format="{{ range \$key, \$value := .NetworkSettings.Networks}}{{\$value.IPAddress}} {{\$key}} $tags $ports{{end}}"
     else
